1292064796119b592662cf2f1679c3791f2329f4
Harry Lanz <lanzharry97@gmail.com>
2026-01-11 18:23:05 +0000

    move neotree setup into plugins

--- init.lua
+++ init.lua
@@ -21,27 +21,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Set up plugins
 require("lazy").setup({
-	{
-	    "nvim-neo-tree/neo-tree.nvim",
-	    branch = "v3.x",
-	    dependencies = {
-	      "nvim-lua/plenary.nvim",
-	      "MunifTanjim/nui.nvim",
-	      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-	    },
-	    lazy = false, -- neo-tree will lazily load itself
-	    config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				filtered_items = {
-					visible = true,
-					hide_dotfiles = false,
-					hide_gitignored = true,
-					hide_ignored = true,
-				},
-			},
-		})
-	    end
-  },
-  require "plugins.colourtheme"
+    require "plugins.neotree",
+    require "plugins.colourtheme"
 })

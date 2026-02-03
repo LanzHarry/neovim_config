d7babc34999797b89daec171fc7fcbf7decfeb78
Harry Lanz <lanzharry97@gmail.com>
2026-02-03 01:00:10 +0000

    add yank highlighting

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,14 +1,23 @@
 local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
 local g_opts = { clear = true }
 
-local user_config_group = augroup("UserConfig", g_opts)
+local filetype_config_group = augroup("FiletypeConfig", g_opts)
+local ux_config_group = augroup("UXConfig", g_opts)
 
 -- turn off auto comment insertion, can be modified per filetype
 vim.api.nvim_create_autocmd("FileType", {
   desc = "Disable auto comment insertion",
-  group = user_config_group,
+  group = filetype_config_group,
   callback = function()
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
+})
+
+-- highlight text when yanking
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking text",
+    group = ux_config_group,
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })

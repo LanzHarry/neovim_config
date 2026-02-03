ba5e432c00b6a2c8f99b276dc6ee56ec9078f022
Harry Lanz <lanzharry97@gmail.com>
2026-02-03 15:13:30 +0000

    reformat lazy setup to match docs more closely

--- init.lua
+++ init.lua
@@ -5,25 +5,28 @@ require("core.autocmds")
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Set up plugins
 require("lazy").setup({
-    require "plugins.colourscheme",
-    require "plugins.bufferline",
-    require "plugins.bbye",
-    require "plugins.neotree",
-    require "plugins.treesitter"
+    spec = {
+        -- import plugins
+        { import = "plugins.colourscheme" },
+        { import = "plugins.bufferline" },
+        { import = "plugins.bbye" },
+        { import = "plugins.neotree" },
+        { import = "plugins.treesitter" }
+    }
 })

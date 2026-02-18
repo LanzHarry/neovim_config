111a973c57ef05d98a29d3cc6a87189afa8c4b75
Harry Lanz <lanzharry97@gmail.com>
2026-02-18 17:10:22 +0000

    add settings to remove vim warnings

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,21 +1,23 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    config = function()
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
           },
         },
-      }
-    },
-    config = function()
-      vim.lsp.config("lua_ls", {})
+      })
       vim.lsp.enable("lua_ls")
     end
   }

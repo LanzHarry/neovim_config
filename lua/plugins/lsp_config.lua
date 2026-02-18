4419265410cf8fe55abd4e28e80a1f2b9f4d3e04
Harry Lanz <lanzharry97@gmail.com>
2026-02-18 19:37:18 +0000

    add deps for mason etc for downloading servers etc

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,7 +1,36 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      { "j-hui/fidget.nvim", opts = {} },
+    },
     config = function()
+      -- following rough structure of kickstart
+      -- could add keymaps in here for go to definition etc
+
+      -- servers to install using mason
+      local servers = {
+        pyright = {},
+      }
+
+      -- ensure servers AND tools are installed here
+      local ensure_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensure_installed, {
+        "lua-language-server", -- note ensure installed here but enable manually
+        "stylua",
+      })
+
+      require("mason-tool-installer").setup {
+        ensure_installed = ensure_installed
+      }
+
+      for server_name, server_settings in pairs(servers) do
+        vim.lsp.config(server_name, server_settings)
+        vim.lsp.enable(server_name)
+      end
+
       vim.lsp.config("lua_ls", {
         settings = {
           Lua = {

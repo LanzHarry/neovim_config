d91074f62567b6a4397c262ffc1319d58f7dbc37
Harry Lanz <lanzharry97@gmail.com>
2026-02-25 12:44:22 +0000

    format telescope lua config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,19 +7,19 @@ return {
     {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install",
-    }
+    },
   },
   config = function()
-    require("telescope").setup{
+    require("telescope").setup {
       extensions = {
         fzf = {
-          fuzzy = true,                    -- false will only do exact matching
-          override_generic_sorter = true,  -- override the generic sorter
-          override_file_sorter = true,     -- override the file sorter
-          case_mode = "smart_case",        -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
-        }
-      }
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
+        },
+      },
     }
     require("telescope").load_extension("fzf")
-  end
+  end,
 }

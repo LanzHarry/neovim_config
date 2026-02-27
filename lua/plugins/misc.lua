2747203ee78eb1dd0ed078726258c646fa379c2d
Harry Lanz <harry.lanz@proton.me>
2026-02-27 17:25:27 +0000

    add which key

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -8,4 +8,17 @@ return {
       fast_wrap = {},
     },
   },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+    },
+    keys = {
+      {
+        "<leader>?",
+        function() require("which-key").show { global = false } end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 }

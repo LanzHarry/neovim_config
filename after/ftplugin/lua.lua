8c623369295ec2682d2ab35f2ae0558eef889aad
Harry Lanz <harry.lanz@proton.me>
2026-03-01 11:40:20 +0000

    Update styling for formatters and live editing

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,4 +1,11 @@
--- settings for language in buffer are local and override defaults
+-- Neovim lua filetype settings
+-- Controls editor typing behaviour, e.g. what is typed when tab is pressed
+-- Settings for language in buffer are local and override defaults
+-- Other source of formatting might come from .editorconfig or *.toml files
+-- This does not control how formatters work (they will typically run on save for example whereas this affects live editing behaviour)
 local set = vim.opt_local
 
-set.shiftwidth = 2
+set.expandtab = true -- insert spaces and not tab characters
+set.shiftwidth = 2 -- indentation width used by >>, <<, =, and autoindent
+set.tabstop = 2 -- how many cols a literal \t displays as
+set.softtabstop = 2 -- how many spaces are inserted when tab or backspace are used in insert mode
